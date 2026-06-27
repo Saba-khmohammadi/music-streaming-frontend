e32ff7f4d8af3cb699c26536225bf59bce1de051
AyehSaberi84 <153058585+AyehSaberi84@users.noreply.github.com>
2026-06-27 21:02:22 +0330

    fix miniplayer fron

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" dir="ltr">
       <body>
+        {/* تزریق مستقیم CDN فونت‌آوسوم نسخه ۶ برای کل پروژه */}
+        <link 
+          rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+          integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+          crossOrigin="anonymous" 
+          referrerPolicy="no-referrer" 
+        />
+
         <AuthProvider>
           <PlayerProvider>
             {children}
